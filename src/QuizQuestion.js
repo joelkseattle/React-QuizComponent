4b12c2d31ec8a89dcf09cfd646cad442f4751d93
Joel Katzevman <joelkseattle@me.com>
2018-06-10 22:10:37 -0700

    fix

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -37,9 +37,9 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
-        this.state.incorrectAnswer ?
+        {this.state.incorrectAnswer ?
         <p className='error'>Sorry, that's not right.</p>
-        : null;
+        : null}
       </main>
     );
   }
